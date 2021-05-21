03f933efa303519996917f0542dbb9f19f2ca8fe
Jinwoo Jeong <jjwlove12@naver.com>
2021-05-21 22:06:24 +0900

    test

--- client.c
+++ client.c
@@ -10,99 +10,119 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 
-
-
 #define DATA_SOCK 0
 #define MSG_SOCK 1
+
 #define MAX_CMD 100
 
-void sendFile(char* name,int socks);
+void sendFile(char *name, int socks);
 void error_handling(char *message);
-int main(int argc, char* argv[]){
-	int serv_sock;
+int main(int argc, char *argv[])
+{
+	int serv_sock[2];
 	//int clnt_sock[2];
-    int str_len, len;
-	struct sockaddr_in serv_addr;
+	int str_len, len;
+	struct sockaddr_in serv_addr[2];
 	char message[MAX_CMD];
-	if(argc!=3){
+	if (argc != 3)
+	{
 		printf("Usage : %s <IP> <PORT> \n", argv[0]);
 		exit(1);
 	}
-	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-    if(serv_sock== -1){
-		error_handling("socket() error");
-	}
-	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family=AF_INET;
-	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
-	serv_addr.sin_port=htons(atoi(argv[2]));
-	printf("serv_addr port : %d \n",atoi(argv[2]));
-	if(connect(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1) {
-		error_handling("connect() error!");
-	}
-	// test
-	str_len=read(serv_sock, message, BUFSIZ-1);
 
-	if(str_len==-1){error_handling("read() error!");}
-		
-	printf("Message from server MSG_SOCK: %s \n", message);
-    char cmd[MAX_CMD];
-	fgets(cmd,MAX_CMD,stdin);
-	sendFile(cmd,serv_sock);
-	printf("file send comp!\n");
-	while(!strcmp(message,"q\n")){
-		fputs("input msg (q to Quit) : ",stdout);
-		fgets(message,BUFSIZ,stdin);
-		write(serv_sock,message,strlen(message));
-		str_len=read(serv_sock, message, BUFSIZ-1);
-		if(str_len==-1){error_handling("read() error!");}
+	for (int i = 0; i < 2; i++)
+	{
+		serv_sock[i] = socket(PF_INET, SOCK_STREAM, 0);
+		if (serv_sock[i] == -1)
+		{
+			error_handling("socket() error");
+		}
+		memset(&serv_addr[i], 0, sizeof(serv_addr[i]));
+		serv_addr[i].sin_family = AF_INET;
+		serv_addr[i].sin_addr.s_addr = inet_addr(argv[1]);
+		serv_addr[i].sin_port = htons(atoi(argv[2])+i);
+		printf("serv_addr port : %d \n", atoi(argv[2])+i);
+		if (connect(serv_sock[i], (struct sockaddr *)&serv_addr[i], sizeof(serv_addr[i])) == -1)
+		{
+			error_handling("connect() error!");
+		}
+		// test
+		str_len = read(serv_sock[MSG_SOCK], message, BUFSIZ - 1);
+
+		if (str_len == -1)
+		{
+			error_handling("read() error!");
+		}
+
+		printf("Message from server MSG_SOCK: %s \n", message);
+		char cmd[MAX_CMD];
+		fgets(cmd, MAX_CMD, stdin);
+		sendFile(cmd, serv_sock[DATA_SOCK]);
+		printf("file send comp!\n");
+	}
+
+	while (!strcmp(message, "q\n"))
+	{
+		fputs("input msg (q to Quit) : ", stdout);
+		fgets(message, BUFSIZ, stdin);
+		write(serv_sock[MSG_SOCK], message, strlen(message));
+		str_len = read(serv_sock[MSG_SOCK], message, BUFSIZ - 1);
+		if (str_len == -1)
+		{
+			error_handling("read() error!");
+		}
 		printf("Message from server MSG_SOCK: %s \n", message);
 	}
 
-	for(int i = 0; i < 1; i++){
+	for (int i = 0; i < 1; i++)
+	{
 		close(serv_sock);
 	}
 	return 0;
 }
 
-void sendFile(char* name,int socks){
+void sendFile(char *name, int socks)
+{
 	size_t fsize, nsize = 0;
 	size_t fsize2;
-    	char * fname = (char*)malloc(strlen(name));
-	strncpy(fname,name,sizeof(name));
+	char *fname = (char *)malloc(strlen(name));
+	strncpy(fname, name, sizeof(name));
 	char buf[BUFSIZ];
-	FILE * file = NULL;
+	FILE *file = NULL;
 	/* 전송할 파일 이름을 작성합니다 */
-	file = fopen(fname,"rb");
-	if(file == NULL){
+	file = fopen(fname, "rb");
+	if (file == NULL)
+	{
 		printf("%s \n", fname);
-		fprintf(stderr,"fopenfail!");
+		fprintf(stderr, "fopenfail!");
 		exit(1);
 	}
 	printf("file open!\n");
-   	/* 파일 크기 계산 */
-   	// move file pointer to end
+	/* 파일 크기 계산 */
+	// move file pointer to end
 	fseek(file, 0, SEEK_END);
 	// calculate file size
-	fsize=ftell(file);
+	fsize = ftell(file);
 	// move file pointer to first
 	fseek(file, 0, SEEK_SET);
 
 	// send file contents
-	while (nsize!=fsize) {
+	while (nsize != fsize)
+	{
 		// read from file to buf
 		// 1byte * 256 count = 256byte => buf[256];
 		int fpsize = fread(buf, 1, 256, file);
 		nsize += fpsize;
-		printf("%d / %d \n",fpsize,nsize);
-		send(socks,buf,fpsize,0);
+		printf("%d / %d \n", fpsize, nsize);
+		send(socks, buf, fpsize, 0);
 	}
 	free(fname);
-	write(socks,"END",strlen("END"));
+	write(socks, "END", strlen("END"));
 	fclose(file);
 }
 
-void error_handling(char *message){
+void error_handling(char *message)
+{
 	fputs(message, stderr);
 	fputc('\n', stderr);
 	exit(1);
