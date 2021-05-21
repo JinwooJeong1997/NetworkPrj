a6a588107d7d5b95f16cbe6fb33cf63e67f0bb7c
Jinwoo Jeong <jjwlove12@naver.com>
2021-05-21 23:43:36 +0900

    문법오류수정

--- server.c
+++ server.c
@@ -41,34 +41,33 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-		//init socks
-		serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-		if (serv_sock == -1)
-		{
-			error_handling("socket() error");
-		}
-		printf("socket[%d] socket() succeed \n ", i);
-		memset(&serv_addr, 0, sizeof(serv_addr));
-		serv_addr.sin_family = AF_INET;
-		serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-		serv_addr.sin_port = htons(atoi(argv[1]));
-		printf("serv_addr port : %d \n", ntohs(serv_addr.sin_port));
+	//init socks
+	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+	if (serv_sock == -1)
+	{
+		error_handling("socket() error");
+	}
+	printf("socket[%d] socket() succeed \n ", i);
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(atoi(argv[1]));
+	printf("serv_addr port : %d \n", ntohs(serv_addr.sin_port));
 
-		if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
-		{
-			error_handling("bind() error");
-		}
-		if (listen(serv_sock, 5) == -1)
-		{
-			error_handling("listen() error");
-		}
+	if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+	{
+		error_handling("bind() error");
+	}
+	if (listen(serv_sock, 5) == -1)
+	{
+		error_handling("listen() error");
+	}
 
-		clnt_addr_size = sizeof(clnt_addr);
-		clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
-		if (clnt_sock == -1)
-		{
-			error_handling("accept() error");
-		}
+	clnt_addr_size = sizeof(clnt_addr);
+	clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
+	if (clnt_sock == -1)
+	{
+		error_handling("accept() error");
 	}
 
 	write(clnt_sock, message, strlen(message) - 1);
