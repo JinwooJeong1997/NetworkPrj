6bb3161c11ee57d73ff3d5bfc3a973b9dfc15f11
Jinwoo Jeong <jjwlove12@naver.com>
2021-05-21 22:06:24 +0900

    test

--- server.c
+++ server.c
@@ -9,7 +9,6 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-
 #define DATA_SOCK 0
 #define MSG_SOCK 1
 
@@ -21,78 +20,90 @@ void error_handling(char *message);
 
 int main(int argc, char *argv[])
 {
-	int serv_sock;
-	int clnt_sock;
+	int serv_sock[2];
+	int clnt_sock[2];
 	int str_len;
 	char buf[256];
-	struct sockaddr_in serv_addr;
-	struct sockaddr_in clnt_addr;
-	socklen_t clnt_addr_size;
+	struct sockaddr_in serv_addr[2];
+	struct sockaddr_in clnt_addr[2];
+	socklen_t clnt_addr_size[2];
 
-	strcpy(message,"hello world!");
-	
-	if(argc!=2){
+	strcpy(message, "hello world!");
+
+	if (argc != 2)
+	{
 		printf("Usage : %s <port>\n", argv[0]);
 		exit(1);
 	}
-	//init socks
-	serv_sock=socket(PF_INET, SOCK_STREAM, 0);
 
-		if(serv_sock == -1 ){
-			error_handling("socket() error");
-		}
-		printf("%d socket succeed \n ",i);
-		memset(&serv_addr, 0, sizeof(serv_addr));
-		serv_addr.sin_family=AF_INET;
-		serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
-		serv_addr.sin_port=htons(atoi(argv[1]));
-		printf("serv_addr[%d] port : %d",i,atoi(argv[1]));
+	for (int i = 0; i < 2; i++)
+	{
+		//init socks
+		serv_sock[i] = socket(PF_INET, SOCK_STREAM, 0);
+		if (serv_sock[i] == -1){error_handling("socket() error");}
+		printf("%d socket succeed \n ", i);
+		memset(&serv_addr[i], 0, sizeof(serv_addr[i]));
+		serv_addr[i].sin_family = AF_INET;
+		serv_addr[i].sin_addr.s_addr = htonl(INADDR_ANY);
+		serv_addr[i].sin_port = htons(atoi(argv[1])+i);
+		printf("serv_addr[%d] port : %d", i, atoi(argv[1]));
 
-		if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr))==-1 ){
+		if (bind(serv_sock[i], (struct sockaddr *)&serv_addr[i], sizeof(serv_addr[i])) == -1)
+		{
 			error_handling("bind() error");
-		} 
-		printf("%d bind() succeed \n ",i);
+		}
+		printf("%d bind() succeed \n ", i);
 
-		if(listen(serv_sock, 5)==-1){
+		if (listen(serv_sock[i], 5) == -1)
+		{
 			error_handling("listen() error");
 		}
-		printf("%d listen() succeed \n ",i);
+		printf("%d listen() succeed \n ", i);
 
-		clnt_addr_size=sizeof(clnt_addr);  
-		clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr,&clnt_addr_size);
-		if(clnt_sock==-1){
-			printf("%d \n ",i);
+		clnt_addr_size[i] = sizeof(clnt_addr[i]);
+		clnt_sock[i] = accept(serv_sock[i], (struct sockaddr *)&clnt_addr[i], &clnt_addr_size[i]);
+		if (clnt_sock[i] == -1)
+		{
+			printf("%d \n ", i);
 			error_handling("accept() error");
 		}
-		printf("%d accept() succeed \n ",i);
-	 
-	
-	write(clnt_sock, message, strlen(message)-1);
+		printf("%d accept() succeed \n ", i);
+
+		write(clnt_sock[MSG_SOCK], message, strlen(message) - 1);
+	}
+
 	int nbyte = 256;
-    size_t filesize = 0, bufsize = 0;
-    FILE *file = NULL;
-   	file = fopen("test.txt", "wb");
-   	bufsize = 256;
-   	while(nbyte!=0) {
-       	nbyte = recv(clnt_sock, buf, bufsize, 0);
-       	fwrite(buf, sizeof(char), nbyte, file);		
-   	}
+	size_t filesize = 0, bufsize = 0;
+	FILE *file = NULL;
+	file = fopen("test.txt", "wb");
+	bufsize = 256;
+	while (nbyte != 0)
+	{
+		nbyte = recv(clnt_sock[DATA_SOCK], buf, bufsize, 0);
+		fwrite(buf, sizeof(char), nbyte, file);
+	}
 	fclose(file);
 	printf("file update complete!\n");
-	while(1){
-		str_len=read(clnt_sock, message, BUFSIZ-1);
-		if(str_len==-1){error_handling("read() error!");}
-		if(!strcmp(message,"q\n") || !strcmp(message,"Q\n")){
+	while (1)
+	{
+		str_len = read(clnt_sock[MSG_SOCK], message, BUFSIZ - 1);
+		if (str_len == -1)
+		{
+			error_handling("read() error!");
+		}
+		if (!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
+		{
+			close(clnt_sock[MSG_SOCK])
 			break;
 		}
-		write(clnt_sock, message, strlen(message));
-		message[str_len]=0;
+		write(clnt_sock[MSG_SOCK], message, strlen(message));
+		message[str_len] = 0;
 		printf("Message from client MSG_SOCK: %s \n", message);
-
-		
 	}
-	close(clnt_sock);	
-	close(serv_sock);
+	for(i = 0; i < 2; i++){
+		close(clnt_sock[i]);
+		close(serv_sock[i]);
+	}
 	return 0;
 }
 
